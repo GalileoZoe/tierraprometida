89ff12f8ee85d23ee482e3c9ff7d2ffbfca26951
GalileoZoe <al222210067@gmail.com>
2024-10-03 17:49:42 -0600

    url edited

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN:WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN@mongodb.railway.internal:27017'),
     StudentsModule,
+    MongooseModule.forRoot('mongodb://mongo:WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN@mongodb.railway.internal:27017/tierraprometida'),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
