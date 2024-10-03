964508dd3e6f7a301812539f5512251bbea129ef
GalileoZoe <al222210067@gmail.com>
2024-10-03 17:27:52 -0600

    route fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { StudentsModule } from './students/students.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongo:WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN@junction.proxy.rlwy.net:44487/tierraprometida'),
+    MongooseModule.forRoot('mongodb://mongo:WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN@mongodb.railway.internal:27017/tierraprometida'),
     StudentsModule,
   ],
   controllers: [AppController],
