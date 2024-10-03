307fc2339a39ab00897f174ea9a5438fcc7ef05f
GalileoZoe <al222210067@gmail.com>
2024-10-03 17:44:01 -0600

    url edited

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { StudentsModule } from './students/students.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN:WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN@mongodb.railway.internal:27017/tierraprometida'),
+    MongooseModule.forRoot('mongodb://WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN:WxfjNbEYetcLoBFmdNpwUJiccKeTrAbN@mongodb.railway.internal:27017'),
     StudentsModule,
   ],
   controllers: [AppController],
