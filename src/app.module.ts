b75f4c4fa5a012cebc12cae8bd34faa0a8346192
GalileoZoe <al222210067@gmail.com>
2024-11-20 11:09:45 -0600

    Production

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,8 @@ import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot('mongodb://mongo:iSYjHUidqXllgiIdvhrCdxydVUyiMvcM@junction.proxy.rlwy.net:49497'),
-    MongooseModule.forRoot('mongodb://localhost:27017/tierraprometida'),
+    MongooseModule.forRoot('mongodb://mongo:iSYjHUidqXllgiIdvhrCdxydVUyiMvcM@junction.proxy.rlwy.net:49497'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/tierraprometida'),
     StudentsModule,
     UsersModule,
     LoginModule,
